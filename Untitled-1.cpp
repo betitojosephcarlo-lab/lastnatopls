0d879b754ce72c3f05dce750dec57b204db3e20c
unknown <betitojosephcarlo@gmail.com>
2025-11-14 18:54:53 +0800

    created a repository for lab a to lab b

---
+++ Untitled-1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // Declare variables of different types
+    int age = 18;                      // Integer variable
+    float height = 6.5f;               // Floating-point variable
+    char grade = 'A';                  // Character variable
+    double FinalOutput = 1.000;        // Double-precision floating-point variable
+    char name[] = "Pogi";            // String variable
+
+    // Display the values of the variables
+    printf("Age: %d\n", age);
+    printf("Height: %.1f\n", height);
+    printf("Grade: %c\n", grade);
+    printf("Final Output: %.4f\n", FinalOutput);
+    printf("Name: %s\n", name);
+    printf("sample statement");
+
+
+    return 0;
+}
